f4b425d7c671892fdf7990004e5161e884c99922
Lihuan Zhou <zhoulihuan@9realms.co>
2025-07-12 22:26:23 +0800

    update

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,9 @@
 const { onCall } = require('firebase-functions/v2/https')
+const { setGlobalOptions } = require("firebase-functions/v2");
+
+setGlobalOptions({
+    serviceAccount: "compute-custom@default-sa-test-90b15.iam.gserviceaccount.com",
+});
 
 const addmessage = onCall((request) => {
     return {
